9f6d92afdf68e3025566fd70a0df9113cb46e122
Thomas Baggi-Sani <thomas.baggi@live.fr>
2025-12-01 14:18:19 +0100

    Modification de la favicon et du titre de la page web

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,19 +9,19 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src="https://media.licdn.com/dms/image/v2/D4E03AQFJAMGCUjHcdQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1665560104814?e=1766016000&v=beta&t=7BCiT631EPQbuGTxeYpmczS5RRUrCAHACroZqiuskhY" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/ThomasFF1987" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/thomasbsani" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>(WIP) Thomas Baggi-Sani</h1>
+          <p>Senior Game Developer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/ThomasFF1987" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/thomasbsani" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
